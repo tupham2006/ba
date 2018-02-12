5c2115fc24a196cae4f05c890712fd9baa7da6b3
tupham <tupham20061995@gmail.com>
2018-02-13 03:46:58 +0700

    reset

--- assets/controllers/EventCtrl.js
+++
@@ -1,4 +0,0 @@
-angular.module('ba')
-	.controller("EvenCtrl", ["$scope", "User", "$rootScope", function($scope, User, $rootScope){
-		$rootScope.activePage = "event";
-	}]);
--- assets/controllers/OverviewCtrl.js
+++
@@ -1,6 +0,0 @@
-angular.module('ba').controller("OverviewCtrl", [
-	"$rootScope",
-	function ($rootScope){
-		$rootScope.activePage = "report";
-	}
-]);
