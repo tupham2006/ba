b49e2f893cd15c6b193824785efa9ab7fb6bb3db
tupham <tupham20061995@gmail.com>
2018-02-13 03:59:32 +0700

    reset

--- assets/filters/dayInWeek.js
+++
@@ -1,5 +0,0 @@
-angular.module('ba').filter('dayInWeek', function(){
-	return function(date){
-		return moment(date).get('days') + 1;
-	};
-});
--- database/clean_table.sql
+++
@@ -1,5 +0,0 @@
-DELETE from borrow;
-DELETE from borrow_book;
-
--- balance book quantity
-UPDATE book SET current_quantity = use_quantity;
